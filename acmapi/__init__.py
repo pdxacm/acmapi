a141d39a055eb24884c94268340e00efc2bffc9f
Cameron Brandon White <cameronbwhite90@gmail.com>
2014-04-10 03:02:40 -0700

    Basic application factory
    Created a basic application factory to bootstrap the project.
    This will need to be expanded as the project grows but this is
    what is needed to start testing.

---
+++ acmapi/__init__.py
@@ -0,0 +1,33 @@
+from flask import Flask
+
+from .database import DB
+
+__version__ = '0.1.0'
+
+def create_app(config_files=None, envvars=None, **other):
+    app = Flask(__name__)
+    
+    for i in iterable(config_files):
+        app.config.from_pyfile(i)
+
+    for i in iterable(envvars): 
+        app.config.from_envvar(i)
+
+    for key, value in other.iteritems():
+        app.config[key] = value
+    
+    #from views import acmweb
+    #app.register_blueprint(acmweb)
+    
+    # database
+    DB.init_app(app)
+    
+    return app
+
+def iterable(x):
+    if x is None:
+        return ()
+    elif not hasattr(x, '__iter__'):
+        return tuple(x)
+    else:
+        return x
