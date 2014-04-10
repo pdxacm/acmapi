dbc8482b9c00018510631ff198b27f5903231bef
Cameron Brandon White <cameronbwhite90@gmail.com>
2014-04-10 03:10:08 -0700

    Setup TravisCI

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 acmapi
 ======
 
+[![Build Status](https://travis-ci.org/cameronbwhite/acmapi.png?branch=master)](https://travis-ci.org/cameronbwhite/acmapi)
+
 API for the @acmpdx
+
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name="acmapi",
+        version="0.1.0",
+        description="",
+        author="Cameron Brandon White",
+        author_email="cameronbwhite90@gmail.com",
+        url="https://github.com/cameronbwhite/acmapi",
+        install_requires = [
+            "Flask",
+            "Flask-SQLAlchemy",
+            "Flask-RESTful",
+        ],
+        test_requires = [
+            "Nose",
+            "Mock",
+        ],
+        include_package_data=True,
+        zip_safe=False,
+    )
